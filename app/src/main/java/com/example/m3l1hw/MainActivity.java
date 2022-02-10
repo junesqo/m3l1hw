f3a5982969888b196f564001ba667a535c27e930
Nurel Sovetbekov <62104475+junesqo@users.noreply.github.com>
2022-02-10 20:21:23 +0600

    new font, glide, no status bar, edit text borders

--- app/src/main/java/com/example/m3l1hw/MainActivity.java
+++ app/src/main/java/com/example/m3l1hw/MainActivity.java
@@ -4,13 +4,24 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView imageCar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imageCar= findViewById(R.id.car);
+        Glide.with(this).load("https://i.pinimg.com/474x/23/ab/a6/23aba60b66ef08174bb7455c4a8a2d2f.jpg").into(imageCar);
+
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
     }
